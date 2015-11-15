35aa99933f5ce091542fbf2c5749032a7daf68c4
Jay Steven Hamilton <hamilton_jay@msn.com>
2015-11-14 20:53:08 -0500

    Small comment modification by Jay to validate git ignore file in project.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TCSAutoSkim.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TCSAutoSkim.java
@@ -13,7 +13,7 @@ public class TCSAutoSkim extends TCSAutoBase {
         super.runOpMode();
 
         // wait for the start button to be pressed
-        //moveStraight(double speed, double seconds, double wait) throws
+        //moveStraight(double speed, double seconds, double wait)
         waitForStart();
 
         pauseRobot(8);
